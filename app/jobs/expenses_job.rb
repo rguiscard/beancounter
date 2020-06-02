e75e2cf7d8e98082fa360aab5d1d64d98340f1ba
Robert Guiscard <robert.guiscard@outlook.com>
2020-06-01 23:07:47 -0400

    fix caching issue for expense

--- app/jobs/expenses_job.rb
+++ app/jobs/expenses_job.rb
@@ -13,7 +13,7 @@ class ExpensesJob < ApplicationJob
     query = query + "WHERE account ~ \"Expenses:*|Income:*\" "
     content =  %x(bean-query -q -f csv #{path} '#{query}')
     if expense = user.expenses.find_or_create_by(year: year, month: month)
-      expense.update_attribute(:details, content)
+      expense.update_attributes(details: content, updated_at: DateTime.current)
     end
   end
 end
