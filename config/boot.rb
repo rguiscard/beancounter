c73c2966278860512538dfbac6c4a52e2ce3b184
Robert Guiscard <robert.guiscard@outlook.com>
2020-06-02 23:47:09 -0400

    gracefully handle no bootsnap for production

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,9 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+begin
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+rescue LoadError
+  # Gracefully handle no bootsnap for production
+  nil
+end
